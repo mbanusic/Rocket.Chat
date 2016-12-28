8eb8349a0c02e535166513c5e0db09b5b637f127
Marko Banusic <mbanusic@gmail.com>
2016-12-28 09:56:26 +0100

    use .sh to build

--- example-build.sh
+++ example-build.sh
@@ -3,11 +3,7 @@ set -x
 set -euvo pipefail
 IFS=$'\n\t'
 
-export METEOR_SETTINGS=$(cat settings.json)
 export NODE_ENV=production
-meteor add rocketchat:internal-hubot meteorhacks:kadira
-meteor build --server https://demo.rocket.chat --directory /var/www/rocket.chat
-cd /var/www/rocket.chat/bundle/programs/server
+meteor build --server https://chat.net.hr --directory /var/www/chat/temp
+cd /var/www/chat/temp/bundle/programs/server
 npm install
-cd /var/www/rocket.chat/current
-pm2 startOrRestart /var/www/rocket.chat/current/pm2.json
